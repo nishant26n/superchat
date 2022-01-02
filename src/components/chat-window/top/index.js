c663e88e1ebc461fdc08b50bffad982f4f0e4d30
Nishant Rajpoot <nishant26n@Nishants-MacBook-Air.local>
2022-01-02 22:01:58 +0530

    started to build chat-top; created room decription model

--- src/components/chat-window/top/index.js
+++ src/components/chat-window/top/index.js
@@ -1,9 +1,39 @@
 import React, { memo } from "react";
+import { Link } from "react-router-dom";
+import { ButtonToolbar } from "rsuite";
 import { useCurrentRoom } from "../../../context/current-room.context";
+import { useMediaQuery } from "../../../misc/custom-hook";
+import RoomInfoBtnModal from "./RoomInfoBtnModal";
 
 const ChatTop = () => {
   const name = useCurrentRoom((v) => v.name);
-  return <div>{name}</div>;
+  const isMobile = useMediaQuery("(max-width: 992px)");
+
+  return (
+    <div>
+      <div className="d-flex justify-content-between align-items-center">
+        <h4>
+          <Link
+            to="/"
+            className={
+              isMobile
+                ? "d-inline-block p-0 mr-2 text-blue link-unstyled"
+                : "d-none"
+            }
+          >
+            <i className="fas fa-arrow-left"></i>
+          </Link>
+          <span className="text-disappear">{name}</span>
+        </h4>
+        <ButtonToolbar className="ws-nowrap">todo</ButtonToolbar>
+      </div>
+
+      <div className="d-flex justify-content-between align-items-center">
+        <span>todo</span>
+        <RoomInfoBtnModal />
+      </div>
+    </div>
+  );
 };
 
-export default ChatTop(memo);
+export default memo(ChatTop);
